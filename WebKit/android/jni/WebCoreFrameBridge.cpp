406baffaa62b814c995768ee5d0af6a22d0eaf93
Dinesh K Garg <dineshg@quicinc.com>
2010-07-15 09:30:09 -0700

    Dns prefetch implementation on Froyo
    Change-Id: Ic0f5f44124573a999821b49479e9a0839cc87fad

--- WebKit/android/jni/WebCoreFrameBridge.cpp
+++ WebKit/android/jni/WebCoreFrameBridge.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright 2006, The Android Open Source Project
+ * Copyright (c) 2010, Code Aurora Forum. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -1583,6 +1584,103 @@ static void OrientationChanged(JNIEnv *env, jobject obj, int orientation)
     pFrame->sendOrientationChangeEvent(orientation);
 }
 
+static bool isInsideViewPort(WebCore::Element *pElem, const WebCore::IntRect & origRect)
+{
+    RenderObject * pRenderer = pElem->renderer();
+    if(!pRenderer)
+        return false;
+
+    FloatPoint absLoc = pRenderer->localToAbsolute();
+    FrameView * pView = pElem->document()->view();
+    return pView && origRect.contains(absLoc.x(), absLoc.y());
+}
+
+static void traverseDOMForViewPort(WebCore::Element* pElem, const WebCore::IntRect & origRect,const KURL& mainURL, WTF::HashMap<WebCore::String, String>& list, int maxHostCount)
+{
+    KURL url;
+    const WebCore::Element *parentElement = pElem;
+
+    if (parentElement->hasAttribute(HTMLNames::hrefAttr)) {
+
+        const AtomicString& attr = parentElement->getAttribute(HTMLNames::hrefAttr);
+        if(attr.startsWith("/")) {
+            url = mainURL;
+        }
+        else {
+            url = KURL(ParsedURLString, attr.string());
+        }
+        if(!url.isEmpty() && url.host().length() > 0){
+           if(isInsideViewPort(pElem, origRect))
+               list.add(url.host(),"1");
+           else
+               list.add(url.host(),"0");
+        }
+    }
+
+    for (WebCore::Element * childElement = parentElement->firstElementChild(); childElement && (list.size() < maxHostCount); childElement = childElement->nextElementSibling())
+        traverseDOMForViewPort(childElement, origRect,mainURL,list,maxHostCount);
+}
+
+static void traverseFrameForViewPort(WebCore::Frame* pFrame, const IntRect& origRect, WTF::HashMap<WebCore::String, String>& hostList,int maxHostCount)
+{
+    const KURL& mainURL = pFrame->loader()->url();
+
+    if((hostList.size() < maxHostCount) && pFrame->document()->isDNSPrefetchEnabled()) {
+        WebCore::Element *parentElement = pFrame->document()->documentElement();
+        traverseDOMForViewPort(parentElement, origRect, mainURL, hostList,maxHostCount);
+    }
+
+    for (Frame* child = pFrame->tree()->firstChild(); child && (hostList.size() < maxHostCount); child = child->tree()->nextSibling())
+        traverseFrameForViewPort(child, origRect, hostList,maxHostCount);
+}
+
+static jobject createJavaMapFromHTTPHosts(JNIEnv* env, const WTF::HashMap<WebCore::String, String>& map)
+{
+    jclass mapClass = env->FindClass("java/util/HashMap");
+    LOG_ASSERT(mapClass, "Could not find HashMap class!");
+
+    jmethodID init = env->GetMethodID(mapClass, "<init>", "(I)V");
+    LOG_ASSERT(ctor, "Could not find constructor for HashMap");
+
+    jobject hashMap = env->NewObject(mapClass, init,map.size());
+    LOG_ASSERT(hashMap, "Could not create a new HashMap");
+
+    jmethodID put = env->GetMethodID(mapClass, "put",
+             "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
+    LOG_ASSERT(put, "Could not find put method on HashMap");
+
+    WTF::HashMap<WebCore::String, String>::const_iterator end = map.end();
+    for (WTF::HashMap<WebCore::String, String>::const_iterator i = map.begin(); i != end; ++i) {
+        if (i->first.length() == 0 )
+            continue;
+        jstring key = env->NewString((jchar *)i->first.characters(), i->first.length());
+        jstring val = env->NewString((jchar *)i->second.characters(), i->second.length());
+        if (key && val) {
+            env->CallObjectMethod(hashMap, put, key, val);
+        }
+        if(key)
+            env->DeleteLocalRef(key);
+        if(val)
+            env->DeleteLocalRef(val);
+    }
+
+    env->DeleteLocalRef(mapClass);
+
+    return hashMap;
+}
+
+static jobject getEmbeddedHostNames(JNIEnv *env, jobject obj, jint maxHostCount)
+{
+    WebCore::Frame* pFrame = GET_NATIVE_FRAME(env, obj);
+    WTF::HashMap<WebCore::String, String> hostList;
+    jobject hashmap = NULL;
+
+    const WebCore::IntRect & origRect = pFrame->view()->platformWidget()->getWindowBounds();
+    traverseFrameForViewPort(pFrame,origRect,hostList,maxHostCount);
+    hashmap = createJavaMapFromHTTPHosts(env,hostList);
+    return hashmap;
+}
+
 // ----------------------------------------------------------------------------
 
 /*
@@ -1634,7 +1732,9 @@ static JNINativeMethod gBrowserFrameNativeMethods[] = {
     { "getFormTextData", "()Ljava/util/HashMap;",
         (void*) GetFormTextData },
     { "nativeOrientationChanged", "(I)V",
-        (void*) OrientationChanged }
+        (void*) OrientationChanged },
+    { "nativeGetEmbeddedHostNames", "(I)Ljava/util/HashMap;",
+        (void*) getEmbeddedHostNames }
 };
 
 int register_webframe(JNIEnv* env)
