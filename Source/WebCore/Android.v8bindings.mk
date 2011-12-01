a83b28bd6d1f6d289d258aac1336c3f047ba226c
Sagar Shah <sagars@codeaurora.org>
2011-12-01 14:10:50 -0800

    Changes to allow for dynamically shared library
    Change-Id: Ia4540d509194371132c0da6172430a8328e8877b

--- Source/WebCore/Android.v8bindings.mk
+++ Source/WebCore/Android.v8bindings.mk
@@ -25,9 +25,15 @@
 
 LOCAL_CFLAGS += -DWTF_USE_V8=1
 
+ifeq ($(DYNAMIC_SHARED_LIBV8SO),true)
 BINDING_C_INCLUDES := \
-	external/v8/include \
-	\
+	vendor/qcom/opensource/v8/include
+else
+BINDING_C_INCLUDES := \
+	external/v8/include
+endif
+
+BINDING_C_INCLUDES += \
 	$(LOCAL_PATH)/bindings/v8 \
 	$(LOCAL_PATH)/bindings/v8/custom \
 	$(LOCAL_PATH)/bindings/v8/specialization \
