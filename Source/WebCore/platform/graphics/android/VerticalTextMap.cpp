e35f14b89ecccf1efb254c3d8dad29e0eaba4ba4
claireho <chinglanho@gmail.com>
2011-12-19 17:16:25 -0800

    Change VerticalTextMap.cpp for VerticalWritingMode after fallback font update. Bug 5472953 : Need glyphs for Vertical Text Form
    1. Enable some mappings after new fallback font(https://android-git.corp.google.com/g/#/c/155813/).
    2. Add more mappings for converting punctuations to their vertical forms.
    3. Error corrections for 0x002d, 0x003b and 0x3019.
    
    Change-Id: I63f2c58af1d7bf6c8b9f5bd93c7a0d9f9f70f691

--- Source/WebCore/platform/graphics/android/VerticalTextMap.cpp
+++ Source/WebCore/platform/graphics/android/VerticalTextMap.cpp
@@ -34,27 +34,30 @@
 
 static const UChar vTextCnvTable[][2] = {
     // TODO: uncomment mappings once we add glyphs for vertical forms.
-    // {0x0021, 0xfe15},  // exclamation mark
+    {0x0021, 0xfe15},  // exclamation mark
     {0x0028, 0xfe35},  // left paren
     {0x0029, 0xfe36},  // right paren
-    // {0x002c, 0xfe10},  // comma
+    {0x002c, 0xfe10},  // comma
+    {0x002d, 0xfe32},  // hyphen
     {0x003a, 0xfe30},  // colon
-    {0x003b, 0x007c},  // hyphen
-    // {0x003f, 0xfe16},  // question mark
-    // {0x005b, 0xfe14},  // semicolon
-    {0x005d, 0xfe47},  // left square bracket
-    {0x005f, 0xfe48},  // right square bracket
+    {0x003b, 0xfe14},  // semicolon
+    {0x003f, 0xfe16},  // question mark
+    {0x005b, 0xfe47},  // left square bracket
+    {0x005d, 0xfe48},  // right square bracket
     {0x007b, 0xfe37},  // left curly bracket
     {0x007d, 0xfe38},  // right curly bracket
     {0x007e, 0x007c},  // tilde to vertical line
+    {0x00ab, 0xfe3d},  // left pointing double angle quotation mark
+    {0x00bb, 0xfe3e},  // right pointing double angle quotation mark
+    {0x2010, 0xfe32},  // hyphen
     {0x2013, 0xfe32},  // en dash
     {0x2014, 0xfe31},  // em dash
     {0x2015, 0xfe31},  // horizontal bar
     {0x2025, 0xfe30},  // two dot leader
-    // TODO: change the mapping 0x2026 -> 0xFE19 once Android has the glyph for 0xFE19.
-    {0x2026, 0xfe30},  // three dot leader
-    // {0x3001, 0xfe11},  // Ideographic comma
-    // {0x3002, 0xfe12},  // Ideographic full stop
+    {0x2026, 0xfe19},  // three dot leader
+    // TODO: change the mapping 0x3001 -> 0xFE11 once Android has the glyph for 0xFE11.
+    {0x3001, 0xfe10},  // Ideographic comma
+    {0x3002, 0xfe12},  // Ideographic full stop
     {0x3008, 0xfe3f},  // left angle bracket
     {0x3009, 0xfe40},  // right angle bracket
     {0x300a, 0xfe3d},  // left double angle bracket
@@ -67,19 +70,22 @@ static const UChar vTextCnvTable[][2] = {
     {0x3011, 0xfe3c},  // right black lenticular bracket
     {0x3014, 0xfe39},  // left black lenticular bracket
     {0x3015, 0xfe3a},  // right tortise shell bracket
-    // {0x3016, 0xfe17},  // left white lenticular bracket
-    // {0x3017, 0xfe18},  // right white lenticular bracket
-    // {0x3019, 0xfe19},  // horizontal ellipses
+    {0x3016, 0xfe17},  // left white lenticular bracket
+    {0x3017, 0xfe18},  // right white lenticular bracket
     {0x30fc, 0x3021},  // prolonged sound
     {0xfe4f, 0xfe34},  // wavy low line
     {0xff08, 0xfe35},  // full width left paren
     {0xff09, 0xfe36},  // full width right paren
+    {0xff0c, 0xfe10},  // full width comma
     {0xff3b, 0xfe47},  // full width left square bracket
     {0xff3d, 0xfe48},  // full width right square bracket
     {0xff5b, 0xfe37},  // full width left curly bracket
+    {0xff5c, 0xfe31},  // fullwidth vertical line
     {0xff5d, 0xfe38},  // full width right curly bracket
-    // {0xff64, 0xfe11},  // halfwidth ideo comma
-    // {0xff61, 0xfe12},  // halfwidth ideo full stop
+    {0xff5e, 0x007c},  // tilde to vertical line
+    // TODO: change the mapping 0xff64 -> 0xFE11 once Android has the glyph for 0xFE11.
+    {0xff64, 0xfe10},  // halfwidth ideo comma
+    {0xff61, 0xfe12},  // halfwidth ideo full stop
 };
 
 namespace WebCore {
