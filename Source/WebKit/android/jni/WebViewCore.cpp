b30a0130ada873a717efc22c14ef29cd6d7d9ab7
Victoria Lease <violets@google.com>
2012-02-22 10:09:37 -0800

    scroll page to current match during find-on-page
     Bug: 6044659
    
    Change-Id: I7e6e59b365aec0b01c41cb80e4c342f39142b867

--- Source/WebKit/android/jni/WebViewCore.cpp
+++ Source/WebKit/android/jni/WebViewCore.cpp
@@ -4434,6 +4434,8 @@ void WebViewCore::findNextOnPage(bool forward)
             m_activeMatch = selection.firstRange();
             m_mainFrame->document()->markers()->setMarkersActive(
                 m_activeMatch.get(), true);
+            m_mainFrame->selection()->revealSelection(
+                ScrollAlignment::alignCenterIfNeeded, true);
         }
         updateMatchCount();
     }
