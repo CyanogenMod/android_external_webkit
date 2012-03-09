df1b448056d65c28220fe303d82847094a215838
John Reck <jreck@google.com>
2012-03-09 15:45:55 -0800

    Delete nativeSubtractLayers
     Bug: 5338388
    
    Change-Id: I2ae12dde6ef2f30e015c052da67c2bafbd59ecd3

--- Source/WebCore/platform/graphics/android/LayerAndroid.cpp
+++ Source/WebCore/platform/graphics/android/LayerAndroid.cpp
@@ -987,43 +987,6 @@ void LayerAndroid::setFixedPosition(FixedPositioning* position) {
     m_fixedPosition = position;
 }
 
-SkRect LayerAndroid::subtractLayers(const SkRect& visibleRect) const
-{
-    SkRect result;
-    if (m_recordingPicture) {
-        // FIXME: This seems wrong. localToGlobal() applies the full local transform,
-        // se surely we should operate globalMatrix on size(), not bounds() with
-        // the position removed? Perhaps we never noticed the bug because most
-        // layers don't use a local transform?
-        // See http://b/5338388
-        SkRect globalRect = bounds();
-        globalRect.offset(-getPosition()); // localToGlobal adds in position
-        SkMatrix globalMatrix;
-        localToGlobal(&globalMatrix);
-        globalMatrix.mapRect(&globalRect);
-        SkIRect roundedGlobal;
-        globalRect.round(&roundedGlobal);
-        SkIRect iVisibleRect;
-        visibleRect.round(&iVisibleRect);
-        SkRegion visRegion(iVisibleRect);
-        visRegion.op(roundedGlobal, SkRegion::kDifference_Op);
-        result.set(visRegion.getBounds());
-#if DEBUG_NAV_UI
-        SkDebugf("%s visibleRect=(%g,%g,r=%g,b=%g) globalRect=(%g,%g,r=%g,b=%g)"
-            "result=(%g,%g,r=%g,b=%g)", __FUNCTION__,
-            visibleRect.fLeft, visibleRect.fTop,
-            visibleRect.fRight, visibleRect.fBottom,
-            globalRect.fLeft, globalRect.fTop,
-            globalRect.fRight, globalRect.fBottom,
-            result.fLeft, result.fTop, result.fRight, result.fBottom);
-#endif
-    } else
-        result = visibleRect;
-    for (int i = 0; i < countChildren(); i++)
-        result = getChild(i)->subtractLayers(result);
-    return result;
-}
-
 void LayerAndroid::dumpLayer(FILE* file, int indentLevel) const
 {
     writeHexVal(file, indentLevel + 1, "layer", (int)this);
