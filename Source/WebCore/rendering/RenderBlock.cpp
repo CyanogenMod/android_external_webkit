f4a8a732d1891dd9477ae706bb4320d5ff3075b7
Steve Block <steveblock@google.com>
2012-04-12 15:38:40 +0100

    Cherry-pick WebKit change r88854 as a prerequisite for r107627
    See http://trac.webkit.org/changeset/88854
    
    Bug: 6328992
    Change-Id: I87cfd2aa0d76dec9b511067ba150db700d297a63

--- Source/WebCore/rendering/RenderBlock.cpp
+++ Source/WebCore/rendering/RenderBlock.cpp
@@ -663,7 +663,7 @@ RenderBlock* RenderBlock::columnsBlockForSpanningElement(RenderObject* newChild)
         && !newChild->isFloatingOrPositioned() && !newChild->isInline() && !isAnonymousColumnSpanBlock()) {
         if (style()->specifiesColumns())
             columnsBlockAncestor = this;
-        else if (parent() && parent()->isRenderBlock())
+        else if (!isInline() && parent() && parent()->isRenderBlock())
             columnsBlockAncestor = toRenderBlock(parent())->containingColumnsBlock(false);
     }
     return columnsBlockAncestor;
