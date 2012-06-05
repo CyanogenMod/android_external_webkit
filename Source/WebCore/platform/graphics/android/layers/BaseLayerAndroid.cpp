87de3ebd26ef37e18c524b32d6673e5462d330b9
Chris Craik <ccraik@google.com>
2012-06-05 14:10:40 -0700

    Reduce base layer false clip size to avoid rounding issues
    bug:6605853
    Change-Id: I65583e61197452ac70c5adf5f967182f0fd7578a

--- Source/WebCore/platform/graphics/android/layers/BaseLayerAndroid.cpp
+++ Source/WebCore/platform/graphics/android/layers/BaseLayerAndroid.cpp
@@ -89,7 +89,8 @@ void BaseLayerAndroid::updatePositionsRecursive(const SkRect& visibleContentRect
     // dynamically increase in size to cover the viewport, and we cache its draw
     // clip. This way the base layer will never have it's visible area clipped
     // by its m_clippingRect, only the viewport.
-    FloatRect clip(0, 0, 1e10, 1e10);
+    // Note: values larger than this suffer from floating point rounding issues
+    FloatRect clip(0, 0, 1e7, 1e7);
 
     bool forcePositionCalculation = !m_positionsCalculated;
     float scale = 1.0f;
