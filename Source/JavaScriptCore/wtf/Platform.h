67ccab07ebc1c48acfca71131f70b384e83734ed
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-06-26 14:32:32 +0200

    webgl: fix build error
    Change-Id: Ic783296ce27481928080b1852934ebc39a3d7bcf

--- Source/JavaScriptCore/wtf/Platform.h
+++ Source/JavaScriptCore/wtf/Platform.h
@@ -707,7 +707,9 @@
 #define ENABLE_SVG 0
 #endif
 #define ENABLE_VIDEO 1
+#ifdef ENABLE_WEBGL
 #define ENABLE_WEBGL 1
+#endif
 #if ENABLE_SVG
 #if !defined(ENABLE_SVG_ANIMATION)
 #define ENABLE_SVG_ANIMATION 0
